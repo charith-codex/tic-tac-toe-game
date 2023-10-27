1e3f5e6ea59fe2605f0a5dad44c35377dd00710c
Charith Wickramasinghe <pvmcharith@gmail.com>
2023-10-27 23:57:54 +0530

    v_1

---
+++ tictac/src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './TicTacToe.css'
+import circle_icon from '../Assets/circle.png'
+import cross_icon from '../Assets/circle.png'
+
+const TicTacToe = () => {
+  return (
+    <div className='container'>
+        
+    </div>
+  )
+}
+
+export default TicTacToe
